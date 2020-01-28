70efdb21af73f8f034d193ac2d913d8f856025af
Paul Grenwood <hello@thegrenwoods.com>
2020-01-28 10:02:24 -0800

    FA Custom URL
    New Method for setting local URL for FA

--- functions.php
+++ functions.php
@@ -14,6 +14,7 @@ require_once 'gravityforms.php';
 
 // Enable Font Awesome 5 Pro
 add_filter( 'fl_enable_fa5_pro', '__return_true' );
+add_filter( 'fl_get_fa5_url', function() { return get_stylesheet_directory_uri() . '/fonts/fontawesome/css/all.min.css'; } );
 
 // Actions
 add_action( 'wp_enqueue_scripts', 'FLChildTheme::enqueue_scripts', 1000 );
